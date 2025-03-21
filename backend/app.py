5565737c64e8f24569a4f4cdeadc60df568e9838
zeno <zeno.p@seznam.cz>
2025-03-21 19:01:18 +0100

    app.py

--- backend/app.py
+++ backend/app.py
@@ -30,4 +30,5 @@ def create_app():
 
 if __name__ == "__main__":
     app = create_app()
-    socketio.run(app, host="0.0.0.0", port=5000, debug=True)  # Pouze socketio.run!
+    app.run(debug=True)
+    socketio.run(app, debug=True)  # Pouze socketio.run!
